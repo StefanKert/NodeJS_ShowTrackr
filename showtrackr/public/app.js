bf34cb25f613917b24796981f3aa3959068297e1
Stefan Kert <stefan.kert@inode.at>
2014-06-09 13:56:49 +0200

    Added locationProvider and routeProvider

--- showtrackr/public/app.js
+++ showtrackr/public/app.js
@@ -1,4 +1,30 @@
 angular.module('MyApp', ['ngCookies', 'ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
-  .config(function() {
-
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(true);
+$routeProvider
+  .when('/', {
+    templateUrl: 'views/home.html',
+    controller: 'MainCtrl'
+  })
+  .when('/shows/:id', {
+    templateUrl: 'views/detail.html',
+    controller: 'DetailCtrl'
+  })
+  .when('/login', {
+    templateUrl: 'views/login.html',
+    controller: 'LoginCtrl'
+  })
+  .when('/signup', {
+    templateUrl: 'views/signup.html',
+    controller: 'SignupCtrl'
+  })
+  .when('/add', {
+    templateUrl: 'views/add.html',
+    controller: 'AddCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
   });
+
+
+  }]);
