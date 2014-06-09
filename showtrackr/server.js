f8d198aa2c104cdff1f336e5f97a1dda91eeaa13
Stefan Kert <stefan.kert@inode.at>
2014-06-09 14:05:04 +0200

    Added mongodb interaction for node.js

--- showtrackr/server.js
+++ showtrackr/server.js
@@ -3,6 +3,8 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs');
 
 var app = express();
 
